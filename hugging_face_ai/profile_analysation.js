dfff247309090d4c679573b8bb63085709fd8744
Skye Jacobs <skyejacobs@casema.nl>
2026-03-10 14:55:13 +0100

    Added a beginning of profile_analysation.js
    Took 20 minutes

---
+++ hugging_face_ai/profile_analysation.js
@@ -0,0 +1,43 @@
+import { InferenceClient } from "@huggingface/inference";
+
+// inference client snippet
+const client = new InferenceClient(process.env.HF_TOKEN);
+
+const chatCompletion = await client.chatCompletion({
+    model: "CohereLabs/tiny-aya-water:cohere",
+    messages: [
+        {
+            role: "user",
+            content: "What is the capital of France?",
+        },
+    ],
+});
+
+// fetch snippet
+async function query(data) {
+    const response = await fetch(
+        "https://router.huggingface.co/v1/chat/completions",
+        {
+            headers: {
+                Authorization: `Bearer ${process.env.HF_TOKEN}`,
+                "Content-Type": "application/json",
+            },
+            method: "POST",
+            body: JSON.stringify(data),
+        }
+    );
+    const result = await response.json();
+    return result;
+}
+
+query({
+    messages: [
+        {
+            role: "user",
+            content: "What is the capital of France?",
+        },
+    ],
+    model: "CohereLabs/tiny-aya-water:cohere",
+}).then((response) => {
+    console.log(JSON.stringify(response));
+});
