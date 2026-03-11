0593798b0c8800af0c09299a94c61532f5b7ef86
Skye Jacobs <skyejacobs@casema.nl>
2026-03-11 12:08:25 +0100

    Added error_handling.js
    Took 24 minutes

--- hugging_face_ai/profile_analysation.js
+++ hugging_face_ai/profile_analysation.js
@@ -3,15 +3,22 @@ import { InferenceClient } from "@huggingface/inference";
 // inference client snippet
 const client = new InferenceClient(process.env.HF_TOKEN);
 
-const chatCompletion = await client.chatCompletion({
-    model: "CohereLabs/tiny-aya-water:cohere",
+let out = "";
+
+const stream = client.chatCompletionStream({
+    model: "CohereLabs/tiny-aya-water",
     messages: [
-        {
-            role: "user",
-            content: "What is the capital of France?",
-        },
+        { role: "user", content: "What is the capital of France?" }
     ],
 });
+
+for await (const chunk of stream) {
+    if (chunk.choices && chunk.choices.length > 0) {
+        const newContent = chunk.choices[0].delta.content;
+        out += newContent;
+        console.log(newContent);
+    }
+}
 
 // fetch snippet
 async function query(data) {
