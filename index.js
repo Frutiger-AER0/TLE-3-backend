0ab860b5844ce10100d2aa3859846fbd1687222b
Skye Jacobs <skyejacobs@casema.nl>
2026-03-17 14:57:41 +0100

    Error fixes
    Took 3 minutes

--- index.js
+++ index.js
@@ -17,8 +17,22 @@ dotenv.config();
 import aiRouter from "./routes/aiRouter.js";
 
 try{
-
     const app = express();
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        next();
+    });
+
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+
+        if (req.method === "OPTIONS") {
+            return res.sendStatus(204);
+        }
+        next();
+    });
 
     //Middelware to support application/JSON content-type
     app.use(express.json());
